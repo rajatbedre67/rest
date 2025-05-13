47d391d0fafe2a8706de8b680c4d050745ce063a
rajatbedre67 <rajatsbedre@gmail.com>
2025-05-14 00:28:06 +0530

    first commit

--- src/components/testimonial/Testimonial.js
+++ src/components/testimonial/Testimonial.js
@@ -16,33 +16,33 @@ const Testimonial = () => {
                         {/* Testimonial 1 */}
                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                             <div className="h-full text-center">
-                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src='./images/vikki.jpg' />
+                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src='./images/image1.webp' />
                                 <p className="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                                 <span className="inline-block h-1 w-10 rounded bg-orange-500 mt-6 mb-4" />
-                                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">VIKKI RATHOD</h2>
-                                <p className="text-gray-500">Senior Product Designer</p>
+                                {/* <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">VIKKI RATHOD</h2> */}
+                                {/* <p className="text-gray-500">Senior Product Designer</p> */}
                             </div>
                         </div>
 
                         {/* Testimonial 2 */}
                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                             <div className="h-full text-center">
-                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src='./images/vikki1.jpg' />
+                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src='./images/image2.webp' />
                                 <p className="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                                 <span className="inline-block h-1 w-10 rounded bg-orange-500 mt-6 mb-4" />
-                                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">AARVI</h2>
-                                <p className="text-gray-500">UI Develeoper</p>
+                                {/* <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">AARVI</h2> */}
+                                {/* <p className="text-gray-500">UI Develeoper</p> */}
                             </div>
                         </div>
 
                         {/* Testimonial 3 */}
                         <div className="lg:w-1/3 lg:mb-0 p-4">
                             <div className="h-full text-center">
-                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src='./images/arun.jpg' />
+                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src='./images/image3.jpg' />
                                 <p className="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                                 <span className="inline-block h-1 w-10 rounded bg-orange-500 mt-6 mb-4" />
-                                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">ARUN KUMAR </h2>
-                                <p className="text-gray-500">CTO</p>
+                                {/* <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">ARUN KUMAR </h2> */}
+                                {/* <p className="text-gray-500">CTO</p> */}
                             </div>
                         </div>
                     </div>
